40b1d27e748733e70277bbf639bf7dacaa7f0177
overOP <cgpradeepop@gmail.com>
2025-09-28 14:56:23 +0545

    we see all blogs and single by id

--- index.js
+++ index.js
@@ -25,6 +25,39 @@ app.get("/", (req, res) => {
   });
 });
 
+
+// Get All Blogs API
+app.get("/getAllBlogs", async (req, res) => {
+  const allBlogs = await Blog.find();
+  try {
+    res.status(200).json({
+      message: "Get All Blogs API",
+      data: allBlogs,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+// Get Single Blog API
+app.get("/getSingleBlog/:id", async (req, res) => {
+  const blogId = req.params.id;
+  const blog = await Blog.findById(blogId);
+  try {
+    res.status(200).json({
+      message: "Get Single Blog API",
+      data: blog,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+// Create Blog API
   app.post("/createBlog", async (req, res) => {
     const title = req.body.title;
     const subTitle = req.body.subTitle;
